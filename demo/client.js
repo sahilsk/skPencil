45331617e65bb9558d7a0390fc35ddba91cf6ecb
sonu kumar <sonukr666@gmail.com>
2014-01-18 23:58:15 +0530

    change structure

---
+++ demo/client.js
@@ -0,0 +1,34 @@
+var 
+	Pencil = require("../skPencil")
+	;
+document.onreadystatechange = function () {	
+	if (document.readyState == "complete") {
+        initSKPencil();
+    }
+}
+
+
+function initSKPencil() {
+	window.Pencil = Pencil;
+	var pencil = new Pencil();
+	window.pencil = pencil;
+	pencil.setWhiteboard( "canvas" );
+	pencil.draw();
+	pencil.brushStyle = {
+        'fill': 'none',
+        'stroke': 'red',
+        'stroke-width': '4px'
+    };
+	pencil.brushStyle.stroke = "blue";	
+	
+	pencil.on("path", function(path){
+		console.log("new path added" );
+	});
+	pencil.on("started", function(pos){
+		console.log("client started at ", pos );
+	});
+	pencil.on("stopped", function(pos){
+		console.log("client stopped at ", pos );
+	});	
+
+}
